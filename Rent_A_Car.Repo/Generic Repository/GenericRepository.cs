cc20c910def3000aa1188bc1b9c2f51a9c2a5096
Ervin Čaušević <34125429+Ecausev@users.noreply.github.com>
2019-06-10 12:45:44 +0200

    Initial commit

---
+++ Rent_A_Car.Repo/Generic Repository/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rent_A_Car.Repo
+{
+    public class GenericRepository<TEntity> : IGenericRepository<> where TEntity : class
+    {
+        protected readonly DbContext Context;
+
+        
+        public GenericRepository(DbContext context)
+        {
+            Context = context;
+
+        }
+        public void Add(TEntity entity)
+        {
+            
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+           
+        }
+
+        public TEntity Get(int id)
+        {
+            
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            
+        }
+
+        public void Remove(TEntity entity)
+        {
+           
+        }
+    }
+}
---
+++ Rent_A_Car.Repo/Generic Repository/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rent_A_Car.Repo
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(object id);
+
+        void Insert(TEntity obj);
+
+        void Update(TEntity obj);
+
+        void Delete(object id);
+
+        void Save();
+
+    }
+}
